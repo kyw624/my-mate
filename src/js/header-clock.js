6ecaaa6d931007ef21dcd2f04d38349dc1f6832e
Y.W KIM <kyw624@gmail.com>
2020-08-22 22:56:16 +0900

    함수 선언 방식 수정

--- src/js/header-clock.js
+++ src/js/header-clock.js
@@ -1,29 +1,29 @@
 const clockContainer = document.querySelector('.header__clock');
 
-const addZero = (timeValue) => {
+function addZero(timeValue) {
   if (timeValue < 10) {
     return `0${timeValue}`;
   }
   return timeValue;
-};
+}
 
-const getTime = () => {
+function getTime() {
   const currentDate = new Date();
   const hour = addZero(currentDate.getHours());
   const minute = addZero(currentDate.getMinutes());
   const second = addZero(currentDate.getSeconds());
 
   return `${hour}:${minute}:${second}`;
-};
+}
 
-const paintClock = () => {
+function paintClock() {
   const currentTime = getTime();
   clockContainer.innerHTML = currentTime;
-};
+}
 
-const init = () => {
+function init() {
   paintClock();
   setInterval(paintClock, 1000);
-};
+}
 
 init();
