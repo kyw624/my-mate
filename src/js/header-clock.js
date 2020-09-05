8043cb74751c4c7a796b28a0affd7c2dc89947e2
Y.W KIM <kyw624@gmail.com>
2020-09-05 18:20:42 +0900

    함수 순서 재배치 및 날짜 표기 기능 추가

--- src/js/header-clock.js
+++ src/js/header-clock.js
@@ -1,29 +1,57 @@
-const clockContainer = document.querySelector('.header__clock');
+function init() {
+  paintClock();
+  paintDate();
+  setInterval(paintClock, 1000);
+}
+
+function paintClock() {
+  const clockContainer = document.querySelector('.header__clock');
+  const currentTime = getDate('time');
+
+  clockContainer.textContent = currentTime;
+}
+
+function paintDate() {
+  const dateContainer = document.querySelector('.content-info-date');
+  const currentDate = getDate('date');
+
+  dateContainer.textContent = currentDate;
+}
+
+function getDate(type) {
+  const currentDate = new Date();
+  let result;
+
+  if (type === 'time') {
+    const hour = addZero(currentDate.getHours());
+    const minute = addZero(currentDate.getMinutes());
+    const second = addZero(currentDate.getSeconds());
+
+    result = `${hour}:${minute}:${second}`;
+
+    // 날짜 넘어가면 갱신
+    if (result === '00:00:00') {
+      paintDate();
+    }
+  } else {
+    const DAY_LIST = ['일', '월', '화', '수', '목', '금', '토'];
+
+    const year = currentDate.getFullYear();
+    const month = currentDate.getMonth() + 1;
+    const date = currentDate.getDate();
+    const day = currentDate.getDay();
+
+    result = `${year}. ${month}. ${date}.(${DAY_LIST[day]})`;
+  }
+
+  return result;
+}
 
 function addZero(timeValue) {
   if (timeValue < 10) {
     return `0${timeValue}`;
   }
   return timeValue;
-}
-
-function getTime() {
-  const currentDate = new Date();
-  const hour = addZero(currentDate.getHours());
-  const minute = addZero(currentDate.getMinutes());
-  const second = addZero(currentDate.getSeconds());
-
-  return `${hour}:${minute}:${second}`;
-}
-
-function paintClock() {
-  const currentTime = getTime();
-  clockContainer.innerHTML = currentTime;
-}
-
-function init() {
-  paintClock();
-  setInterval(paintClock, 1000);
 }
 
 init();
